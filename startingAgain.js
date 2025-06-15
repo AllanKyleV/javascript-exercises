6b11a870704dcd97766b7e55e6754ffdaebf2b16
Allan Kyle <199327511+AllanKyleV@users.noreply.github.com>
2025-06-15 21:09:55 +0800

    more challenge added

--- startingAgain.js
+++ startingAgain.js
@@ -15,17 +15,35 @@ console.log(reverseString('hello'));
 
 // Capitalize the First Letter
 function capitalizeFirstLetter(str) {
-    let result = [];
-    const arr =  str.split('');
+    // let result = [];
+    // const arr =  str.split('');
 
-    for (let i = 0; i < arr.length; i++) {
-        if (i === 0) {
-            result.push(arr[i].toUpperCase());
-        } else {
-            result.push(arr[i].toLowerCase());
-        }
-    }
+    // for (let i = 0; i < arr.length; i++) {
+    //     if (i === 0) {
+    //         result.push(arr[i].toUpperCase());
+    //     } else {
+    //         result.push(arr[i].toLowerCase());
+    //     }
+    // }
 
-    return result.join('');
+    // return result.join('');
+
+    return str[0].toUpperCase() + str.slice(1).toLowerCase();
 }
-console.log(capitalizeFirstLetter('javascript'))
+console.log(capitalizeFirstLetter('javascript'));
+    
+// Check for Palindrome
+function isPalindrome(str) {
+    // let reversedStr = str.split('').reverse().join('');
+    // if (reversedStr === str) {
+    //     return true;
+    // } else {
+    //     return false;
+    // }
+    return str === str.split('').reverse().join('');
+}
+console.log(isPalindrome('racecar'));
+console.log(isPalindrome('hello'));
+
+// Goal, return true if palindrome, else false
+// reverse the string, if the reversed str is equal to the original str, return true, else false.
